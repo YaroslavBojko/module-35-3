22095aee4cb60fa4fb4d25c1b7e12f38c613d182
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-10-16 10:42:50 +0300

    recursive_directory_iterator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <filesystem>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    for(auto &p : std::filesystem::recursive_directory_iterator("S:\\Slillbox\\test 35.3"))
+    {
+        std::cout << p.path() << std::endl;
+    }
+
 }
