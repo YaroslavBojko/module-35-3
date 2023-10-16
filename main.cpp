418c95518c25dd8653744b7836be595d42c63bb2
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-10-16 14:31:53 +0300

    end

--- main.cpp
+++ main.cpp
@@ -2,27 +2,17 @@
 #include <filesystem>
 
 int main() {
-
     std::filesystem::path path("S:\\Slillbox\\test 35.3");
     std::string extension = ".cpp";
 
-    for(auto &p : std::filesystem::recursive_directory_iterator("S:\\Slillbox\\test 35.3"))
-    {
-        std::cout << p.path() << " ";
-        std::filesystem::path ob = p.path();
-        std::cout << is_regular_file(ob) << std::endl;
-        if(is_regular_file(ob))
-        {
-            std::cout << ob.extension() << std::endl;
-            if(!ob.extension().compare(ex))
-                std::cout << "Yes!!!" << std::endl;
-            else std::cout << "No!!!" << std::endl;
-        }
-    }
-
     auto recursiveGetFileNamesByExtension = [](std::filesystem::path path, const std::string extension)
     {
-
+        for(auto &p : std::filesystem::recursive_directory_iterator(path))
+        {
+                if(!p.path().extension().compare(extension))
+                    std::cout << p.path().filename() << std::endl;
+        }
     };
 
+    recursiveGetFileNamesByExtension(path, extension);
 }
